1f710835638767ed1a85705fd841d83e5a5fbabc
Dawson Vaught <vaught.dawson@gmail.com>
2022-04-05 14:16:24 -0700

    Chaining Methods Assignment

--- fundamentals/oop/user.py
+++ fundamentals/oop/user.py
@@ -6,11 +6,13 @@ class User:
 
     def make_deposit(self, amount):
         self.account_balance += amount
+        return self
 
     def make_withdrawal(self, amount):
         if (self.account_balance - amount < 0):
             return print('Error: Balance can not go below 0.')
         self.account_balance -= amount
+        return self
 
     def display_user_balance(self):
         return f'User: {self.name}, Balance: {self.account_balance}'
@@ -20,27 +22,22 @@ class User:
             return print('Error: Balance can not go below 0.')
         self.make_withdrawal(amount)
         user.make_deposit(amount)
+        return self
 
 
 account1 = User('Dawson', 'temp1@mail.com')
 account2 = User('Joe', 'temp2@mail.com')
 account3 = User('Person', 'temp3@mail.com')
 
-account1.make_deposit(3)
-account1.make_deposit(50)
-account1.make_deposit(200)
+account1.make_deposit(3).make_deposit(50).make_deposit(200)
 print(account1.display_user_balance())
 
-account2.make_deposit(1)
-account2.make_deposit(1)
-account2.make_withdrawal(1)
-account2.make_withdrawal(2)  # Error, cannot go below zero
+account2.make_deposit(1).make_deposit(1).make_withdrawal(
+    1).make_withdrawal(2)  # Error, cannot go below zero
 print(account2.display_user_balance())
 
-account3.make_deposit(1000)
-account3.make_withdrawal(100)
-account3.make_withdrawal(50)
-account3.make_withdrawal(25)
+account3.make_deposit(1000).make_withdrawal(
+    100).make_withdrawal(50).make_withdrawal(25)
 print(account3.display_user_balance())
 
 account1.transfer_money(account3, 100)
