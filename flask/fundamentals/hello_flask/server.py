eeb61d80d38a8eae595e1068df493cdbd29bee8e
Dawson Vaught <vaught.dawson@gmail.com>
2022-04-07 20:05:45 -0700

    More template Rendering Practice

--- flask/fundamentals/hello_flask/server.py
+++ flask/fundamentals/hello_flask/server.py
@@ -18,5 +18,16 @@ def hello(banana, num):
     return f'Hello {banana * num}'
 
 
+@app.route('/lists')
+def lists():
+    student_info = [
+        {'name': 'Michael', 'age': 35},
+        {'name': 'John', 'age': 30},
+        {'name': 'Mark', 'age': 25},
+        {'name': 'KB', 'age': 27}
+    ]
+    return render_template("lists.html", random_numbers=[3, 1, 5], students=student_info)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
