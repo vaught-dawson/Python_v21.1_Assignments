cd4b12832d908908c05e6de1e175a2f1c2711772
Dawson Vaught <vaught.dawson@gmail.com>
2022-04-07 18:05:52 -0700

    Rendering Views Practice

--- flask/fundamentals/hello_flask/server.py
+++ flask/fundamentals/hello_flask/server.py
@@ -1,13 +1,22 @@
-from flask import Flask  # Import Flask to allow us to create our app
-# Create a new instance of the Flask class called "app"
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
-# The "@" decorator associates this route with the function immediately following
 @app.route('/')
 def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
+    return render_template('index.html')
 
 
-if __name__ == "__main__":   # Ensure this file is being run directly and not from a different module
-    app.run(debug=True)    # Run the app in debug mode.
+@app.route('/success')
+def success():
+    return 'Success!'
+
+
+@app.route('/hello/<string:banana>/<int:num>')
+def hello(banana, num):
+    return f'Hello {banana * num}'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
