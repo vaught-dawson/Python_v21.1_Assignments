9715aa02e945ead3141b01f99352aa8c9c9f3888
Dawson Vaught <vaught.dawson@gmail.com>
2022-04-07 19:59:40 -0700

    Added slashed to routes

--- flask/fundamentals/checkerboard/server.py
+++ flask/fundamentals/checkerboard/server.py
@@ -9,22 +9,22 @@ def initial_checkerboard():
     return render_template('index.html', color1='red', color2='black', width=8, height=8)
 
 
-@app.route('/<int:height>')
+@app.route('/<int:height>/')
 def height_restricted_checkerboard(height):
     return render_template('index.html', color1='red', color2='black', width=8, height=height)
 
 
-@app.route('/<int:height>/<int:width>')
+@app.route('/<int:height>/<int:width>/')
 def custom_size_checkerboard(height, width):
     return render_template('index.html', color1='red', color2='black', width=width, height=height)
 
 
-@app.route('/<int:height>/<int:width>/<string:color1>')
+@app.route('/<int:height>/<int:width>/<string:color1>/')
 def custom_size_one_color_checkerboard(height, width, color1):
     return render_template('index.html', color1=color1, color2='black', width=width, height=height)
 
 
-@app.route('/<int:height>/<int:width>/<string:color1>/<string:color2>')
+@app.route('/<int:height>/<int:width>/<string:color1>/<string:color2>/')
 def custom_size_two_color_checkerboard(height, width, color1, color2):
     return render_template('index.html', color1=color1, color2=color2, width=width, height=height)
 
